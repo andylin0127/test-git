b17843f210cfc9cd71a2e41d866eb9c8991f834f
andylin0127 <a4308552002@gmail.com>
2025-07-31 12:03:07 +0800

    簡化程式，改寫subtitle監聽

--- personal.js
+++ personal.js
@@ -4,7 +4,11 @@ function innertitle() {
   document.querySelector(".top-inner-title").innerHTML = "首頁";
 }
 
-function innersubtitle1() {
+document.querySelectorAll(".top-inner-subtitle").forEach((item) => {
+  item.addEventListener("click", () => innersubtitle(item));
+});
+
+function innersubtitle(clickedElement) {
   const content = document.querySelector(".content");
   const toptitle = document.querySelector(".top-inner-title");
   document.querySelector(".myself").style.display = "none";
@@ -16,55 +20,20 @@ function innersubtitle1() {
       } else return res.json();
     })
     .then((data) => {
-      content.innerHTML = data.story;
+      if (clickedElement.textContent.trim() === "我的自傳") {
+        content.innerHTML = data.story;
+      }
+      if (clickedElement.textContent.trim() === "我的經歷") {
+        content.innerHTML = data.experience;
+      }
+      if (clickedElement.textContent.trim() === "我的作品") {
+        content.innerHTML = data.product;
+      }
     })
     .catch((error) => {
       console.error("錯誤:", error);
       content.innerHTML = "資料載入失敗，請稍後再試。";
     });
 
-  toptitle.innerHTML = "返回首頁";
-}
-
-function innersubtitle2() {
-  const content = document.querySelector(".content");
-  const toptitle = document.querySelector(".top-inner-title");
-  document.querySelector(".myself").style.display = "none";
-  content.style.display = "block";
-  fetch("data.json")
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("資料讀取失敗");
-      } else return res.json();
-    })
-    .then((data) => {
-      content.innerHTML = data.experience;
-    })
-    .catch((error) => {
-      console.error("錯誤:", error);
-      content.innerHTML = "資料載入失敗，請稍後再試。";
-    });
-
-  toptitle.innerHTML = "返回首頁";
-}
-
-function innersubtitle3() {
-  const content = document.querySelector(".content");
-  const toptitle = document.querySelector(".top-inner-title");
-  document.querySelector(".myself").style.display = "none";
-  content.style.display = "block";
-  fetch("data.json")
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("資料讀取失敗");
-      } else return res.json();
-    })
-    .then((data) => {
-      content.innerHTML = data.product;
-    })
-    .catch((error) => {
-      console.error("錯誤:", error);
-      content.innerHTML = "資料載入失敗，請稍後再試。";
-    });
   toptitle.innerHTML = "返回首頁";
 }
